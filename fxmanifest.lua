6769037343631ab40005dda9a35ea3b8ddf415a1
Gold14567 <70439470+Gold14567@users.noreply.github.com>
2022-12-02 19:00:24 +0100

    Update fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'bodacious'
+game 'gta5'
+
+name "RollDice"
+description "An optimised and standalone RollDice system for FiveM."
+author "SpecialStos"
+version "1.0.0"
+
+client_scripts {
+    'config.lua',
+    'client/RollDice.lua',
+}
+
+server_scripts {
+	'@oxmysql/lib/MySQL.lua',
+    'config.lua',
+    'server/RollDice.lua',
+}
+
